dec93a8f26f55fbc085ee00cb06e4836e024ed3e
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-13 15:10:13 -0300

    feat (first commit): cria estrutura-base do projeto

---
+++ finn/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, status
+
+from finn.schemas import Message
+
+app = FastAPI()
+
+
+@app.get(
+    "/hello",
+    status_code=status.HTTP_200_OK,
+    response_model=Message,
+)
+def hello():
+    return {"message": "hello world!"}
---
+++ tests/test_routes.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from finn.app import app
+from finn.schemas import Message
+
+HELLO_URL = "/hello"
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_hello(client: TestClient):
+    response = client.get(url=HELLO_URL)
+    message_schema = Message.model_validate({"message": "hello world!"}).model_dump()
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == message_schema
