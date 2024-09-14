0731613253eca249f7839dcd73f114d5a8f0e867
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-13 21:34:35 -0300

    feat: resolve erro do local.yml e adiciona rota de criacao de usuário

--- finn/app.py
+++ finn/app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, status
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from finn.schemas.base_schema import Message
+from finn.database import get_session
+from finn.models import User
+from finn.schemas.base_schema import Message, UserCreate, UserPublic
 
 app = FastAPI()
 
@@ -12,3 +16,33 @@ app = FastAPI()
 )
 def hello():
     return {"message": "hello world!"}
+
+
+@app.post(
+    "/users/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserPublic,
+    tags=["users"],
+)
+async def create_user(user: UserCreate, session: AsyncSession = Depends(get_session)):
+    db_user = User(**user.model_dump())
+
+    existent_user = await session.scalar(select(User).where((User.username == user.username) | (User.email == user.email)))
+
+    if existent_user:
+        if existent_user.username == user.username:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User with the same username exists",
+            )
+        elif existent_user.email == user.email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User with the same email exists",
+            )
+
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+
+    return db_user
--- finn/schemas/base_schema.py
+++ finn/schemas/base_schema.py
@@ -1,5 +1,52 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
 
 
 class Message(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     message: str
+
+
+class UserBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    full_name: str
+    password: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserPublic(BaseModel):
+    """Modelo usado no retorno de dados do usuário"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    full_name: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserCreate(BaseModel):
+    """Modelo usado na criação de dados do usuário"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    username: str
+    email: str
+    full_name: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    """Modelo usado na atualização de dados do usuário"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    username: str
+    full_name: str
+    password: str
