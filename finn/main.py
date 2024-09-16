0f3c6e6de832c551e5ed868599e886cd331bc2ce
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-16 11:12:41 -0300

    feat: resolve erro de BIOS (cors não configurado corretamente)

--- finn/main.py
+++ finn/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from finn.categories.router import category_router
 from finn.debit.router import debit_router
 from finn.users.router import user_router
 
 app = FastAPI()
+
+app.include_router(user_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
 
 app.include_router(user_router)
 app.include_router(debit_router)
