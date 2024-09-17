b3cffa760499d75ffde33f93442b0c0c76afa463
ryofac <ryanfaustinocarvalho@gmail.com>
2024-09-16 21:04:01 -0300

    feat: resolvendo erro de BIOS ( CORS )

--- finn/main.py
+++ finn/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from finn.categories.router import category_router
 from finn.debit.router import debit_router
 from finn.users.router import user_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Finn APP",
+    description="API da sua aplicação de finanças",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 app.include_router(user_router)
 app.include_router(debit_router)
